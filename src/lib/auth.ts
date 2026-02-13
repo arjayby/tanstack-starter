6563088062b0e10c1b4656bc7516925c2eb9c104
Arjay Bayona <arjayby1@gmail.com>
2026-02-14 00:11:20 +0800

    feat(auth): add better-auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,21 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
+import { db } from "@/db";
+
+export const auth = betterAuth({
+	database: drizzleAdapter(db, {
+		provider: "pg",
+	}),
+	emailAndPassword: {
+		enabled: true,
+	},
+	socialProviders: {
+		google: {
+			enabled: true,
+			clientId: process.env.GOOGLE_CLIENT_ID as string,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+		},
+	},
+	plugins: [tanstackStartCookies()], // make sure tanstackStartCookies is the last plugin in the array
+});
