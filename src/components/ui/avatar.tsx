142a3900fafb2c2ad3d442695adffcc1be8a5890
Arjay Bayona <arjayby1@gmail.com>
2026-02-21 23:46:23 +0800

    feat(ui): add size xl to avatar component

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -8,14 +8,14 @@ function Avatar({
 	size = "default",
 	...props
 }: React.ComponentProps<typeof AvatarPrimitive.Root> & {
-	size?: "default" | "sm" | "lg";
+	size?: "default" | "sm" | "lg" | "xl";
 }) {
 	return (
 		<AvatarPrimitive.Root
 			data-slot="avatar"
 			data-size={size}
 			className={cn(
-				"size-8 rounded-full after:rounded-full data-[size=lg]:size-10 data-[size=sm]:size-6 after:border-border group/avatar relative flex shrink-0 select-none after:absolute after:inset-0 after:border after:mix-blend-darken dark:after:mix-blend-lighten",
+				"size-8 rounded-full after:rounded-full data-[size=xl]:size-16 data-[size=lg]:size-10 data-[size=sm]:size-6 after:border-border group/avatar relative flex shrink-0 select-none after:absolute after:inset-0 after:border after:mix-blend-darken dark:after:mix-blend-lighten",
 				className,
 			)}
 			{...props}
